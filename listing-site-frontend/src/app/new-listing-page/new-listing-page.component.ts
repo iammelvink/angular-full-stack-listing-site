758a2a0f279616d29b96e97aebd58b65d7557c7a
Melvin Kisten <iammelvink@gmail.com>
2020-12-23 23:05:40 +0200

    created new listing page

--- listing-site-frontend/src/app/new-listing-page/new-listing-page.component.ts
+++ listing-site-frontend/src/app/new-listing-page/new-listing-page.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
-  selector: 'app-new-listing-page',
-  templateUrl: './new-listing-page.component.html',
-  styleUrls: ['./new-listing-page.component.css']
+    selector: 'app-new-listing-page',
+    templateUrl: './new-listing-page.component.html',
+    styleUrls: ['./new-listing-page.component.css']
 })
 export class NewListingPageComponent implements OnInit {
+    // Member variables that contain the name, description and price of new listing
+    name: string = '';
+    description: string = '';
+    price: string = '';
 
-  constructor() { }
+    constructor(
+        // Provider is to go to a route
+        private router: Router,
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    // Call when submit button is clicked
+    onSubmit(): void {
+        // Show alert
+        alert('Creating a new listing...');
+        // Then go to /my-listings
+        this.router.navigateByUrl('/my-listings');
+    }
 
 }
