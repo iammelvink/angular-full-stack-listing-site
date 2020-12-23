061ab6761c68d36a6b4954c6cf5bc537b4baaccf
Melvin Kisten <iammelvink@gmail.com>
2020-12-23 16:33:16 +0200

    created contact seller page

--- listing-site-frontend/src/app/app.module.ts
+++ listing-site-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,20 +12,21 @@ import { NewListingPageComponent } from './new-listing-page/new-listing-page.com
 import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListingsPageComponent,
-    ListingDetailPageComponent,
-    ContactPageComponent,
-    MyListingsPageComponent,
-    NewListingPageComponent,
-    EditListingPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ListingsPageComponent,
+        ListingDetailPageComponent,
+        ContactPageComponent,
+        MyListingsPageComponent,
+        NewListingPageComponent,
+        EditListingPageComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- listing-site-frontend/src/app/contact-page/contact-page.component.ts
+++ listing-site-frontend/src/app/contact-page/contact-page.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
-  selector: 'app-contact-page',
-  templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.css']
+    selector: 'app-contact-page',
+    templateUrl: './contact-page.component.html',
+    styleUrls: ['./contact-page.component.css']
 })
 export class ContactPageComponent implements OnInit {
+    // Member variables that contain the email, message and listing
+    email: string = '';
+    message: string = '';
+    // listing: Listing; not working for now
+    listing: Listing | any;
 
-  constructor() { }
+    constructor(
+        // Provider is to get id param from URL
+        private route: ActivatedRoute,
+        // Provider is to go to a route
+        private router: Router,
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        // Get id param from URL
+        const id = this.route.snapshot.paramMap.get('id');
+        // Assign matching listing to member variable
+        this.listing = fakeListings.find(listing => listing.id === id);
+        // Set default message
+        this.message = `Hi, I'm interested in your ${this.listing.name.toLocaleLowerCase()}!`;
+    }
+
+    // Call when click send button
+    sendMessage(): void {
+        // Show alert
+        alert('Your message has been sent!');
+        // Then go to /listings
+        this.router.navigateByUrl('/listings');
+    }
 
 }
