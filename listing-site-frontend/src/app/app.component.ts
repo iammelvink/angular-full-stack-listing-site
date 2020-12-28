0119800acc84ac0248784f2a66d4f14fb71118f4
Melvin Kisten <iammelvink@gmail.com>
2020-12-28 18:49:21 +0200

    Sign in and out with Google

--- listing-site-frontend/src/app/app.component.ts
+++ listing-site-frontend/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+// import { auth } from 'firebase/app'; changed
+import firebase from 'firebase/app';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listing-site-frontend';
+    title = 'listing-site-frontend';
+
+    constructor(
+        // Injecting AngularFireAuth into the app.component
+        // via the constructor
+        public firebase: AngularFireAuth,
+    ) { }
+
+    // Sign in with Google account
+    signInClicked(): void {
+        this.firebase.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    }
+
+    // Sign out
+    signOutClicked(): void {
+        this.firebase.signOut();
+    }
 }
